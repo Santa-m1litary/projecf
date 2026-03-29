1a37d7bea2a624ec2b4629e3eedfd492dfaaaca6
maxim <mstrahov555@gmail.com>
2026-03-29 06:04:13 -0500

    update

---
+++ tournament/src/main/java/com/joust/tournament/models/Post.java
@@ -0,0 +1,17 @@
+package com.joust.tournament.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    public Post() {}
+}
